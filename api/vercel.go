c3d36882fda0fb5bbbe6a2b875479cce5440521b
raflypsr <raflynvd16@gmail.com>
2024-11-17 21:17:49 +0700

    first commit

--- api/vercel.go
+++ api/vercel.go
@@ -7,7 +7,6 @@ import (
 	"backend-tugas-reactjs/utils"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -32,15 +31,10 @@ func init() {
 	docs.SwaggerInfo.Title = "Book REST API"
 	docs.SwaggerInfo.Description = "This is REST API Book."
 	docs.SwaggerInfo.Version = "1.0"
-	if environment == "development" {
-		docs.SwaggerInfo.Host = utils.Getenv("HOST", "localhost:9090")
-		docs.SwaggerInfo.Schemes = []string{"http", "https"}
-	} else {
-		docs.SwaggerInfo.Host = os.Getenv("HOST")
-		docs.SwaggerInfo.Schemes = []string{"https"}
-	}
-	db := config.ConnectDataBase()
+	docs.SwaggerInfo.Host = utils.Getenv("HOST", "localhost:9090")
+	docs.SwaggerInfo.Schemes = []string{"http"}
 
+	db := config.ConnectDataBase()
 	routes.SetupRouter(db, App)
 }
 
